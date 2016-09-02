051461d7d0c906f9260a0eba6ae5479d5ac50e60
John Cobb <john@cphandheld.com>
2016-09-02 11:49:37 -0500

    updated setup.sh to include modifications to /boot/cmdline.txt and /boot/config.txt

--- setup.sh
+++ setup.sh
@@ -36,12 +36,18 @@ sudo cp /home/pi/apps/modemscripts/etc/ppp/options /etc/ppp/options
 sudo cp /home/pi/apps/modemscripts/etc/ppp/peers/* /etc/ppp/peers
 sudo cp /home/pi/apps/modemscripts/etc/ppp/ip-up.d/addroute /etc/ppp/ip-up.d/addroute
 
+echo "updating /boot/cmdline.txt"
+cp /boot/cmdline.txt /boot/cmdline.txt.bak
+sed -i -e 's/console=serial0/console=tty0/g' /boot/cmdline.txt
+echo "updating /boot/config.txt"
+cp /boot/config.txt /boot/config.txt.bak
+echo "enable_uart=1" >> /boot/config.txt
 
 #git clone https://github.com/johncobb/modemscripts.git /home/pi/apps/modemscripts
 
 #git clone https://github.com/johncobb/modemscripts.git /home/pi/apps/modemscripts
 # copy service descriptor to systemd folder
-cp /home/pi/apps/modemscripts/systemd/enablemodem.service /etc/systemd/system/enablemodem.service
+sudo cp /home/pi/apps/modemscripts/systemd/enablemodem.service /etc/systemd/system/enablemodem.service
 
 systemctl enable enablemodem.service
 systemctl start enablemodem.service
